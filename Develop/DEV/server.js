4ce2e9c00b326c932697519f211467b21681f70f
Ant Buffill <abuffill@outlook.com>
2024-01-26 00:38:43 -0600

    changes to files

---
+++ Develop/DEV/server.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const path = require('path');
+const fs = require('fs');
+const notes = require('./db/db.json');
+const PORT = process.env.PORT || 3001;
+const { v4: uuidv4 } = require('uuid');
+
+const app = express();
+
+app.use(express.static('public'));
+app.use(express.json())
+// GET Route for homepage
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
+
+app.get('/api/notes', (req, res) =>
+   res.json(notes)
+);
+
+app.post('/api/notes', (req, res) => {
+  req.body.id = uuidv4();
+  notes.push(req.body);
+  fs.writeFileSync('./db/db.json', JSON.stringify(notes));
+  res.json(notes);
+
+});
+app.delete('/api/notes/:id', (req, res) => {
+  for (let i = 0; i < notes.length; i++) {
+  if (notes[i].id== req.params.id) {
+    notes.splice(i, 1)
+  }
+  }
+  fs.writeFileSync('./db/db.json', JSON.stringify(notes));
+  res.json(notes);
+
+});
+
+app.listen(PORT, () => console.log(`App listening on  http://localhost:${PORT}`));
